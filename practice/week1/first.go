e76221929638da52db444ee487f2ea6a58debb69
shuvo-halder <samshuvo00@gmail.com>
2025-09-18 23:56:17 +0600

    Go

--- practice/week1/first.go
+++ practice/week1/first.go
@@ -8,5 +8,7 @@ func main(){
 	// define veriable
 
 	firstVeriable := " My name is Sam Shuvo"
+	var secondVeriable = "hey shuvo number id: 10"
 	fmt.Println(firstVeriable)
+	fmt.Println(secondVeriable)
 }
